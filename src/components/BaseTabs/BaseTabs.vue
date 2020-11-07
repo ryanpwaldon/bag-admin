4e5c9c6d7f92bcb0e06df17295645e90582c2e27
Ryan Waldon <ryanpwaldon@gmail.com>
2020-11-07 22:42:43 +1100

    feat: tabs

---
+++ src/components/BaseTabs/BaseTabs.vue
@@ -0,0 +1,44 @@
+<template>
+  <nav class="flex space-x-8">
+    <div
+      :key="i"
+      v-for="(tab, i) in tabs"
+      :class="
+        tab.name === activeTab
+          ? 'border-gray-900 text-gray-900'
+          : 'text-gray-500 hover:text-gray-700 border-transparent hover:border-gray-300 focus:border-gray-300 focus:text-gray-700'
+      "
+      class="inline-flex items-center px-1 py-4 text-sm font-medium leading-5 transition duration-150 ease-in-out border-b-2 cursor-pointer select-none group"
+      @click="$emit('update:activeTab', tab.name)"
+    >
+      <component
+        :is="tab.icon"
+        :class="tab.name === activeTab ? 'text-gray-500' : 'text-gray-400 group-hover:text-gray-500 group-focus:text-gray-500'"
+        class="flex-shrink-0 w-6 h-6 mr-3 -ml-1 transition duration-150 ease-in-out"
+      />
+      <span>{{ tab.name }}</span>
+    </div>
+  </nav>
+</template>
+
+<script lang="ts">
+import { Component, defineComponent, PropType } from 'vue'
+
+export interface Tab {
+  name: string
+  icon: Component
+}
+
+export default defineComponent({
+  props: {
+    activeTab: {
+      type: String,
+      required: true
+    },
+    tabs: {
+      type: Object as PropType<Tab[]>,
+      required: true
+    }
+  }
+})
+</script>
--- src/views/Offers/views/CrossSells/views/CrossSell/CrossSell.vue
+++ src/views/Offers/views/CrossSells/views/CrossSell/CrossSell.vue
@@ -2,10 +2,10 @@
   <BaseHeader />
   <BaseLoader v-if="loading" />
   <div v-else>
-    <BaseCard>
-      <div class="grid gap-3">
+    <div class="overflow-hidden bg-white divide-y divide-gray-200 rounded-lg shadow">
+      <div class="px-4 py-5 sm:p-6">
         <h2 class="text-lg font-medium leading-6 text-gray-800">{{ item?.product.title }}</h2>
-        <div class="flex space-x-4 text-sm font-medium leading-4 text-gray-500">
+        <div class="flex mt-3 space-x-4 text-sm font-medium leading-4 text-gray-500">
           <div class="flex items-center space-x-1">
             <Tag class="w-5 h-5" />
             <p>Cross sell</p>
@@ -21,31 +21,28 @@
           <BaseBadge :text="item?.active ? 'Live' : 'Paused'" :theme="item?.active ? 'green' : 'yellow'" />
         </div>
       </div>
-    </BaseCard>
-    <div class="grid gap-6 mt-6 lg:grid-cols-12">
-      <BaseSidebarTabs :tabs="tabs" v-model:activeTab="activeTab" class="lg:col-span-3" />
-      <div class="grid gap-6 lg:col-span-9">
-        <template v-if="activeTab === 'Overview'">
-          <BaseStats
-            :stats="[
-              { label: 'Total income', value: '$29.00' },
-              { label: 'Conversions', value: '2' },
-              { label: 'Impressions', value: '2093' }
-            ]"
-          />
-        </template>
-        <template v-if="activeTab === 'Edit'">
-          <StatusForm v-model:item="item" />
-          <CopyForm v-model:item="item" />
-          <TriggersForm v-model:item="item" />
-        </template>
-      </div>
+      <BaseTabs :tabs="tabs" v-model:activeTab="activeTab" class="px-4 sm:px-6" />
+    </div>
+    <div class="grid gap-6 mt-6">
+      <template v-if="activeTab === 'Overview'">
+        <BaseStats
+          :stats="[
+            { label: 'Total income', value: '$29.00' },
+            { label: 'Conversions', value: '2' },
+            { label: 'Impressions', value: '2093' }
+          ]"
+        />
+      </template>
+      <template v-if="activeTab === 'Edit'">
+        <StatusForm v-model:item="item" />
+        <CopyForm v-model:item="item" />
+        <TriggersForm v-model:item="item" />
+      </template>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import BaseCard from '@/components/BaseCard/BaseCard.vue'
 import BaseHeader from '@/components/BaseHeader/BaseHeader.vue'
 import BaseLoader from '@/components/BaseLoader/BaseLoader.vue'
 import BaseBadge from '@/components/BaseBadge/BaseBadge.vue'
@@ -53,7 +50,7 @@ import BaseStats from '@/components/BaseStats/BaseStats.vue'
 import StatusForm from './components/StatusForm/StatusForm.vue'
 import CopyForm from './components/CopyForm/CopyForm.vue'
 import TriggersForm from './components/TriggersForm/TriggersForm.vue'
-import BaseSidebarTabs, { Tab } from '@/components/BaseSidebarTabs/BaseSidebarTabs.vue'
+import BaseTabs, { Tab } from '@/components/BaseTabs/BaseTabs.vue'
 import crossSellService, { CrossSell } from '@/services/api/services/crossSellService'
 import Calendar from '@/icons/Calendar.vue'
 import Clock from '@/icons/Clock.vue'
@@ -72,11 +69,10 @@ export default defineComponent({
     BaseHeader,
     BaseLoader,
     BaseBadge,
-    BaseSidebarTabs,
+    BaseTabs,
     StatusForm,
     CopyForm,
     TriggersForm,
-    BaseCard,
     Calendar,
     BaseStats,
     Clock,
