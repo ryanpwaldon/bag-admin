0fe31317fb53753e1c54d64e0682ebcdae4f3d83
Ryan Waldon <ryanpwaldon@gmail.com>
2020-11-15 10:45:04 +1100

    feat: style

--- src/components/BaseTabs/BaseTabs.vue
+++ src/components/BaseTabs/BaseTabs.vue
@@ -5,13 +5,13 @@
         :class="
           isActive
             ? 'border-gray-900 text-gray-900'
-            : 'text-gray-500 hover:text-gray-700 border-transparent hover:border-gray-300 focus:border-gray-300 focus:text-gray-700'
+            : 'text-gray-500 hover:text-gray-900 border-transparent hover:border-gray-300 focus:border-gray-300 focus:text-gray-900'
         "
         class="inline-flex items-center px-1 py-4 text-sm font-medium leading-5 transition duration-150 ease-in-out border-b-2 cursor-pointer select-none group"
       >
         <component
           :is="link.icon"
-          :class="isActive ? 'text-gray-500' : 'text-gray-400 group-hover:text-gray-500 group-focus:text-gray-500'"
+          :class="isActive ? 'text-gray-800' : 'text-gray-400 group-hover:text-gray-500 group-focus:text-gray-500'"
           class="flex-shrink-0 w-5 h-5 mr-2 -ml-1 transition duration-150 ease-in-out"
         />
         <span>{{ link.title }}</span>
