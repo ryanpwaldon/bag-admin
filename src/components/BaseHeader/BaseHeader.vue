731820d99725c27e12b87d23bfe150a7f9de98b9
Ryan Waldon <ryanpwaldon@gmail.com>
2020-11-15 10:21:45 +1100

    feat: responsiveness

--- src/components/BaseHeader/BaseHeader.vue
+++ src/components/BaseHeader/BaseHeader.vue
@@ -1,13 +1,13 @@
 <template>
   <header class="mb-8">
     <BaseBreadcrumbs class="mb-2" />
-    <div class="md:flex md:items-center md:justify-between">
+    <div class="flex items-center justify-between">
       <div class="flex-1 min-w-0">
         <h2 class="text-2xl font-semibold leading-7 text-gray-700 sm:truncate">
           {{ $route.meta.title }}
         </h2>
       </div>
-      <div v-if="$slots.actions" class="flex flex-shrink-0 mt-4 md:mt-0 md:ml-4">
+      <div v-if="$slots.actions" class="flex flex-shrink-0 ml-4">
         <slot name="actions" />
       </div>
     </div>
