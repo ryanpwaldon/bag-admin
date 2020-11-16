031ba001fef151d99ccd68adfca83c28c9e254fb
Ryan Waldon <ryanpwaldon@gmail.com>
2020-11-16 13:20:42 +1100

    feat: empty table fallback

--- src/views/Offers/views/CrossSells/CrossSells.vue
+++ src/views/Offers/views/CrossSells/CrossSells.vue
@@ -5,6 +5,7 @@
     :items="items"
     :link="item => ({ name: 'cross-sell', params: { id: item.id } })"
     class="overflow-hidden bg-white rounded-lg shadow"
+    v-if="loading || items.length"
   >
     <template #product="{ item }">
       <div class="text-sm font-medium leading-5 text-gray-900">{{ item.product.title }}</div>
@@ -22,6 +23,13 @@
       <BasePagination :loading="loading" :total="total" v-model:page="page" :pages="pages" :limit="limit" />
     </template>
   </BaseTable>
+  <BaseGridCard v-else>
+    <div class="flex flex-col items-center justify-center h-44">
+      <img class="h-10" src="@/assets/img/empty-box.svg" />
+      <p class="mt-2 text-base font-medium leading-6 text-gray-700">You have not created any cross sells</p>
+      <BaseButton class="mt-3" text="Create a cross sell" />
+    </div>
+  </BaseGridCard>
 </template>
 
 <script lang="ts">
@@ -29,11 +37,15 @@ import { defineComponent, watchEffect, ref } from 'vue'
 import BaseTable from '@/components/BaseTable/BaseTable.vue'
 import BasePagination from '@/components/BasePagination/BasePagination.vue'
 import crossSellService, { CrossSell } from '@/services/api/services/crossSellService'
+import BaseGridCard from '@/components/BaseGridCard/BaseGridCard.vue'
+import BaseButton from '@/components/BaseButton/BaseButton.vue'
 export default defineComponent({
   name: 'Offers',
   components: {
     BaseTable,
-    BasePagination
+    BasePagination,
+    BaseGridCard,
+    BaseButton
   },
   setup() {
     const page = ref(1)
