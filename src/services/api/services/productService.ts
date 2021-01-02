4037c069cbdaf807db9b506fccf0619099c496cd
Ryan Waldon <ryanpwaldon@gmail.com>
2021-01-03 08:31:28 +1100

    chore: cleanup

--- src/services/api/services/productService.ts
+++ src/services/api/services/productService.ts
@@ -1,12 +1,7 @@
 import { client } from '@/services/api/client'
 import { AdminProduct } from '@/types/admin/types'
-import { parseGid } from '@shopify/admin-graphql-api-utilities'
 
 export default {
-  async findOneById(id: string): Promise<AdminProduct> {
-    return (await client({ url: `/product/${parseGid(id)}`, method: 'get' })).data
-  },
-
   async findByIds(ids: string[]): Promise<AdminProduct[]> {
     return (await client({ url: `/product/ids`, method: 'get', params: { ids } })).data
   }
