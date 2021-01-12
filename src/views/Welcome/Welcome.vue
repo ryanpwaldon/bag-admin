142c5acecd0952bd3694b38a366c9a5c07c9979e
Ryan Waldon <ryanpwaldon@gmail.com>
2021-01-12 16:42:26 +1100

    feat: copy

--- src/views/Welcome/Welcome.vue
+++ src/views/Welcome/Welcome.vue
@@ -3,7 +3,7 @@
     <Logo class="h-5" />
     <div class="flex flex-col px-6 py-8 mt-8 overflow-hidden rounded-lg shadow bg-gradient-to-tr from-blue-700 to-blue-400">
       <h1 class="text-2xl font-semibold leading-8 text-white md:text-3xl">Welcome</h1>
-      <p class="mt-2 text-base leading-6 text-blue-100 md:text-lg">Excited to have you on board!<br />Select a plan to get started.</p>
+      <p class="mt-2 text-base leading-6 text-blue-100 md:text-lg">We're excited to have you on board!<br />Select a plan to get started.</p>
     </div>
     <div class="mt-8">
       <BaseSubscriptions />
