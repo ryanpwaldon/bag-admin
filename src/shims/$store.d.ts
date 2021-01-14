580e6af95fec9c7efda6835f8a31a31230d037e6
Ryan Waldon <ryanpwaldon@gmail.com>
2021-01-14 14:11:17 +1100

    chore: updates

--- src/shims/$store.d.ts
+++ src/shims/$store.d.ts
@@ -1,7 +1,8 @@
+import store from '@/store/store'
 import { ComponentCustomProperties } from 'vue'
 
 declare module '@vue/runtime-core' {
   interface ComponentCustomProperties {
-    $store: any
+    $store: typeof store
   }
 }
