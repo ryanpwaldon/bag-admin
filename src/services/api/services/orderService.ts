fcfde756711a9b2636adb521be3c42a5a2dd16ac
Ryan Waldon <ryanpwaldon@gmail.com>
2021-02-02 18:24:06 +1100

    chore: remove orderService

--- src/services/api/services/orderService.ts
+++
@@ -1,8 +0,0 @@
-import { client } from '@/services/api/client'
-import { AdminOrder } from '@/types/admin/types'
-
-export default {
-  async findByIds(ids: string[]): Promise<AdminOrder[]> {
-    return (await client({ url: `/order/ids`, method: 'get', params: { ids } })).data
-  }
-}
