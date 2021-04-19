f9bc3b2fa3a28ad787dc4a41330cb57cb9ee3549
Ryan Waldon <ryanpwaldon@gmail.com>
2021-04-19 11:20:41 +0930

    chore: add $shopify typings

--- src/plugins/shopify.ts
+++ src/plugins/shopify.ts
@@ -7,34 +7,40 @@ import { ResourcePicker } from '@shopify/app-bridge/actions'
 import getShopOriginFromUrl from '@/utils/getShopOriginFromUrl'
 import { Section } from '@shopify/app-bridge/actions/Navigation/Redirect'
 
+const createShopifyPlugin = (shopOrigin: string) => {
+  const shopifyAppBridge = createApp({ apiKey: process.env.VUE_APP_SHOPIFY_API_KEY, shopOrigin })
+  const shopifyRedirect = Redirect.create(shopifyAppBridge)
+  const shopifyProductPicker = ResourcePicker.create(shopifyAppBridge, { resourceType: ResourcePicker.ResourceType.Product })
+  return {
+    getSessionToken() {
+      return getSessionToken(shopifyAppBridge)
+    },
+    redirectToAdminUrl(section: Section) {
+      shopifyRedirect.dispatch(Redirect.Action.ADMIN_SECTION, section)
+    },
+    redirectToExternalUrl(url: string) {
+      shopifyRedirect.dispatch(Redirect.Action.REMOTE, url)
+    },
+    async openProductPicker({ selectMultiple = false, showVariants = false }) {
+      shopifyProductPicker.set({ selectMultiple, showVariants })
+      shopifyProductPicker.dispatch(ResourcePicker.Action.OPEN)
+      const selection: { id: string }[] = await new Promise(resolve => {
+        shopifyProductPicker.subscribe(ResourcePicker.Action.SELECT, ({ selection }) => resolve(selection))
+        shopifyProductPicker.subscribe(ResourcePicker.Action.CANCEL, () => resolve([]))
+      })
+      shopifyProductPicker.unsubscribe()
+      const productIds = selection.map(({ id }) => id)
+      return productIds
+    }
+  }
+}
+
+export type Shopify = ReturnType<typeof createShopifyPlugin>
+
 export default {
   install: (app: App) => {
     const shopOrigin = getShopOriginFromUrl()
     if (!shopOrigin || !isFramed) return console.warn('Shopify app bridge could not be created.')
-    const shopifyAppBridge = createApp({ apiKey: process.env.VUE_APP_SHOPIFY_API_KEY, shopOrigin })
-    const shopifyRedirect = Redirect.create(shopifyAppBridge)
-    const shopifyProductPicker = ResourcePicker.create(shopifyAppBridge, { resourceType: ResourcePicker.ResourceType.Product })
-    app.config.globalProperties.$shopify = {
-      getSessionToken() {
-        return getSessionToken(shopifyAppBridge)
-      },
-      redirectToAdminUrl(section: Section) {
-        shopifyRedirect.dispatch(Redirect.Action.ADMIN_SECTION, section)
-      },
-      redirectToExternalUrl(url: string) {
-        shopifyRedirect.dispatch(Redirect.Action.REMOTE, url)
-      },
-      async openProductPicker({ selectMultiple = false, showVariants = false }) {
-        shopifyProductPicker.set({ selectMultiple, showVariants })
-        shopifyProductPicker.dispatch(ResourcePicker.Action.OPEN)
-        const selection: { id: string }[] = await new Promise(resolve => {
-          shopifyProductPicker.subscribe(ResourcePicker.Action.SELECT, ({ selection }) => resolve(selection))
-          shopifyProductPicker.subscribe(ResourcePicker.Action.CANCEL, () => resolve([]))
-        })
-        shopifyProductPicker.unsubscribe()
-        const productIds = selection.map(({ id }) => id)
-        return productIds
-      }
-    }
+    app.config.globalProperties.$shopify = createShopifyPlugin(shopOrigin)
   }
 }
--- src/shims/$shopify.d.ts
+++ src/shims/$shopify.d.ts
@@ -1,7 +1,8 @@
+import { Shopify } from '@/plugins/shopify'
 import { ComponentCustomProperties } from 'vue'
 
 declare module '@vue/runtime-core' {
   interface ComponentCustomProperties {
-    $shopify: any
+    $shopify: Shopify
   }
 }
