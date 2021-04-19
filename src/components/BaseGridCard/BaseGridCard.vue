15c20ef0fe6c7b8bed8879d37218f5ed3b7e8e4c
Ryan Waldon <ryanpwaldon@gmail.com>
2021-04-19 09:35:41 +0930

    feat: update styles

--- src/components/BaseGridCard/BaseGridCard.vue
+++ src/components/BaseGridCard/BaseGridCard.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="overflow-hidden bg-white rounded-lg shadow">
+  <div class="bg-white rounded-lg shadow">
     <div class="px-4 py-5 border-b border-gray-200 sm:px-6 sm:py-5" v-if="$slots.header">
       <slot name="header" />
     </div>
     <div :class="contentPadding && 'px-4 py-5 sm:p-6'">
       <slot />
     </div>
-    <div class="px-4 py-5 mt-2 border-t border-gray-200 sm:px-6 sm:py-5 bg-gray-50" v-if="$slots.footer">
+    <div class="px-4 py-5 mt-2 border-t border-gray-200 rounded-b-lg sm:px-6 sm:py-5 bg-gray-50" v-if="$slots.footer">
       <slot name="footer" />
     </div>
   </div>
