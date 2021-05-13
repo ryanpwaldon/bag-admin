fd0db3f4c927fd83496725229e04b566d61dc804
Ryan Waldon <ryanpwaldon@gmail.com>
2021-05-13 17:46:12 +0930

    feat: restore progress bar conversion stats

--- src/views/Offers/views/ProgressBars/views/ProgressBar/views/Activity/Activity.vue
+++ src/views/Offers/views/ProgressBars/views/ProgressBar/views/Activity/Activity.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="grid gap-6">
+    <ConversionStats :progress-bar="progressBar" />
     <ConversionsTable :progress-bar="progressBar" />
   </div>
 </template>
@@ -7,9 +8,11 @@
 <script lang="ts">
 import { defineComponent, PropType } from 'vue'
 import { ProgressBar } from '@/services/api/services/progressBarService'
+import ConversionStats from './components/ConversionStats/ConversionStats.vue'
 import ConversionsTable from './components/ConversionsTable/ConversionsTable.vue'
 export default defineComponent({
   components: {
+    ConversionStats,
     ConversionsTable
   },
   props: {
--- src/views/Offers/views/ProgressBars/views/ProgressBar/views/Activity/components/ConversionStats/ConversionStats.vue
+++ src/views/Offers/views/ProgressBars/views/ProgressBar/views/Activity/components/ConversionStats/ConversionStats.vue
@@ -1,21 +1,48 @@
 <template>
-  <div></div>
-  <!-- <BaseStats
+  <BaseStatsLoader v-if="loading" />
+  <BaseStats
+    v-else
     :stats="[
-      { label: 'Total Orders', value: ordersOverActivePeriod },
-      { label: 'Total Conversions', value: conversions.length },
+      { label: 'Total Orders', value: totalOrdersOverActivePeriod },
+      { label: 'Total Conversions', value: totalConversions },
       { label: 'Conversion Rate', value: conversionRate }
     ]"
-  /> -->
+  />
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
 import useFormatter from '@/composables/useFormatter'
+import BaseStats from '@/components/BaseStats/BaseStats.vue'
+import orderService from '@/services/api/services/orderService'
+import { ProgressBar } from '@/services/api/services/progressBarService'
+import { computed, defineComponent, PropType, ref, watchEffect } from 'vue'
+import BaseStatsLoader from '@/components/BaseStatsLoader/BaseStatsLoader.vue'
+import conversionService, { ConversionType } from '@/services/api/services/conversionService'
 export default defineComponent({
-  setup() {
+  components: { BaseStats, BaseStatsLoader },
+  props: {
+    progressBar: {
+      type: Object as PropType<ProgressBar>,
+      required: true
+    }
+  },
+  setup(props) {
+    const loading = ref(true)
+    const totalConversions = ref(0)
     const { format } = useFormatter()
-    return { format }
+    const totalOrdersOverActivePeriod = ref(0)
+    const conversionRate = computed(() => {
+      if (!totalOrdersOverActivePeriod.value || !totalConversions.value) return '–'
+      return format.percent(totalConversions.value / totalOrdersOverActivePeriod.value)
+    })
+    const fetchItems = async () => {
+      loading.value = true
+      totalOrdersOverActivePeriod.value = await orderService.countByDateRanges(props.progressBar.activeHistory)
+      totalConversions.value = await conversionService.getTotalCountByOffer(props.progressBar.id, ConversionType.ProgressBar)
+      loading.value = false
+    }
+    watchEffect(fetchItems)
+    return { format, loading, totalOrdersOverActivePeriod, totalConversions, conversionRate }
   }
 })
 </script>
