636f603d4fe9c1c0447bc5f4ee5ec0220a06136e
Ryan Waldon <ryanpwaldon@gmail.com>
2021-07-16 00:55:11 +1000

    fix: unable to create offers after upgrade

--- src/views/Offers/views/Create/Create.vue
+++ src/views/Offers/views/Create/Create.vue
@@ -2,7 +2,7 @@
   <BaseHeader />
   <BaseLoader v-if="loading" />
   <div class="grid gap-6" v-else>
-    <BaseUpgradePrompt v-if="$store.state.user?.subscription === 'Free (v1)' && displayUpgradePrompt" />
+    <BaseUpgradePrompt v-if="displayUpgradePrompt" />
     <BaseGridCard>
       <h3 class="mb-6 text-lg font-medium text-gray-800">Select offer type</h3>
       <BaseInputRadioGroup name="offerType" :options="options" v-model="selected">
@@ -37,6 +37,7 @@ import BaseUpgradePrompt from '@/components/BaseUpgradePrompt/BaseUpgradePrompt.
 import crossSellService from '@/services/api/services/crossSellService'
 import progressBarService from '@/services/api/services/progressBarService'
 import BaseLoader from '@/components/BaseLoader/BaseLoader.vue'
+import { useStore } from 'vuex'
 
 enum RouteNames {
   CrossSell = 'create-cross-sell',
@@ -70,20 +71,22 @@ export default defineComponent({
   },
   setup() {
     const selected = ref()
-    const router = useRouter()
+    const store = useStore()
     const loading = ref(true)
+    const router = useRouter()
     const displayUpgradePrompt = ref(false)
+    const userIsOnFreePlan = store.state.user?.subscription === 'Free (v1)'
     const handleNext = () => router.push({ name: selected.value })
     const totalCrossSells = ref(0 as number)
     const totalProgressBars = ref(0 as number)
     const fetchTotals = async () => {
       totalCrossSells.value = (await crossSellService.findAll({ page: 1, limit: 1 })).total
-      if (totalCrossSells.value >= 1) {
+      if (totalCrossSells.value >= 1 && userIsOnFreePlan) {
         options[0].disabled = true
         displayUpgradePrompt.value = true
       }
       totalProgressBars.value = (await progressBarService.findAll({ page: 1, limit: 1 })).total
-      if (totalProgressBars.value >= 1) {
+      if (totalProgressBars.value >= 1 && userIsOnFreePlan) {
         options[1].disabled = true
         displayUpgradePrompt.value = true
       }
